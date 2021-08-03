44431982626059231c238b28ef79e8a184747c98
kanishk <kanishk.srivastava72@gmail.com>
2021-08-03 23:21:49 +0530

    basket app commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <header className="block row center" style={{backgroundColor:'#edc842'}}>
+      <div className="Head-title">
+      <i class="fas fa-shopping-basket" style={{fontSize:'600%'}}></i>
+        <a href="#/" style={{color:'black'}}>
+          <h1>Hello Basket Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart" style={{color:'black'}}>
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        <a href="#/signin" style={{color:'black', margin:'20px'}}> SignIn</a>
+      </div>
+    </header>
+  );
+}
